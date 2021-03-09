99dae40ca2fd7d57783fa817363639c2aed98493
Elena Zelenina <zelenina.elena@gmail.com>
2021-03-09 10:07:56 -0500

    filled the photo table

--- java/database/photo_table_data.sql
+++ java/database/photo_table_data.sql
@@ -1,6 +1,10 @@
 INSERT INTO photo (user_id, file_name, link, caption, likes_count, date_time)
-VALUES (42, 'photo 1 name', 'https://picsum.photos/id/237/800/450.webp', 'this is the best picture', 25, '2017-01-13T17:09:42.411');
+VALUES (42, 'photo 1 name', 'https://techelevatorgrambucket.s3.amazonaws.com/aphoward/united-states-best-places-to-visit-houston-skyline.jpg', 'this is the best picture', 25, '2017-01-13T17:09:42.411');
 INSERT INTO photo (user_id, file_name, link, caption, likes_count, date_time)
-VALUES (2, 'photo 2 name', 'https://images.unsplash.com/photo-1544717305-2782549b5136', 'this is a good picture', 25, '2018-01-13T17:09:42.411');
+VALUES (2, 'photo 2 name', 'https://techelevatorgrambucket.s3.amazonaws.com/aphoward/ggb.jpg','just a picture' , 25, '2018-01-13T17:09:42.411');
 INSERT INTO photo (user_id, file_name, link, caption, likes_count, date_time)
-VALUES (4, 'photo 3 name', 'https://images.unsplash.com/photo-1571260899304-425eee4c7efc', 'this is a nice picture', 25, '2019-01-13T17:09:42.411');
+VALUES (4, 'photo 3 name', 'https://techelevatorgrambucket.s3.amazonaws.com/aphoward/usa-best-places-washington.jpg', 'this is a nice picture', 25, '2019-01-13T17:09:42.411');
+INSERT INTO photo (user_id, file_name, link, caption, likes_count, date_time)
+VALUES (3, 'photo 3 name', 'https://techelevatorgrambucket.s3.amazonaws.com/aphoward/usa-best-places-waikiki.jpg', 'nice picture', 15, '2020-01-13T17:09:42.411');
+INSERT INTO photo (user_id, file_name, link, caption, likes_count, date_time)
+VALUES (4, 'photo 3 name', 'https://techelevatorgrambucket.s3.amazonaws.com/aphoward/united-states-best-places-to-visit-grand-canyon-lookout.jpg', 'picture', 20, '2021-01-13T17:09:42.411');
