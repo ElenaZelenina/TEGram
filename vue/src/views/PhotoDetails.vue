14caea894aec4ce1537d78a258008b290a9e0ef4
Ebony Sampson <ebonysampson03@gmail.com>
2021-03-10 20:56:16 -0500

    PhotoDetails Page In Progress

---
+++ vue/src/views/PhotoDetails.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+<h1>Photo Details</h1>
+
+</template>
+
+<script>
+import photoFeed from "../views/PhotoFeed";
+
+export default {
+name: "photoDetails",
+  data() {
+    return {
+        photo: {
+            caption: "",
+            uploadDate: "",
+        }
+    };   
+},
+methods: {
+    photoDetail() {
+        let photo = {
+            "caption": "",
+            "uploadDate": new Date('yyyy-MM-dd'),
+        }
+        photos.unshift(photo);
+    }
+},
+}
+</script>
+
+<style scoped>
+
+</style>
