30d7e3336d0d22ace03272c7033a8c5f962d27de
April Howard <apehow111@gmail.com>
2021-03-18 01:05:57 -0400

    work on nav buttons

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,96 +1,93 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div class="navbar">
       <img class="navbar-brand" src="../public/mark-blue.jpg" alt="logo" />
-        
-        <router-link
-          v-bind:to="{ name: 'home' }"
-          class="navbar-item"
+      <div class="navbar">
+        <router-link v-bind:to="{ name: 'home' }" class="navbar-item"
           >Upload Photo</router-link
         >
-        <router-link 
-          v-bind:to="{ name: 'photo-feed' }" 
-          class="navbar-item"
+        <router-link v-bind:to="{ name: 'photo-feed' }" class="navbar-item"
           >Photos</router-link
         >
-        <router-link 
-          v-bind:to="{ name: 'favorites' }" 
-          class="navbar-item"
+        <router-link v-bind:to="{ name: 'favorites' }" class="navbar-item"
           >Favorites</router-link
         >
-       
+
         <router-link
           v-bind:to="{ name: 'logout' }"
           v-if="$store.state.token != ''"
           class="navbar-item"
           >Logout</router-link
         >
-
       </div>
       <img class="navbar-icon" src="../public/TEGram.png" alt="logo" />
     </div>
     <router-view />
   </div>
 </template>
-// use input textbox under the picture--after upload photo it pops up , we type something
-// same click event as submit
-// 2 different tasks talk to aws, send the photo to aws s3, get the link from aws
-// link to the photo which we are storing in the database, and then send all this info
-// to the data base. link file name and caption.
-//??/ andrews
-
-// elena created photoRequest
-// send from client to server, not send whole photo class, which
-
-// something to think about-generate frandom file name, on serve name generate the name there.
-// environment variables for aws
+// use input textbox under the picture--after upload photo it pops up , we type
+something // same click event as submit // 2 different tasks talk to aws, send
+the photo to aws s3, get the link from aws // link to the photo which we are
+storing in the database, and then send all this info // to the data base. link
+file name and caption. //??/ andrews // elena created photoRequest // send from
+client to server, not send whole photo class, which // something to think
+about-generate frandom file name, on serve name generate the name there. //
+environment variables for aws
 
 <style scoped>
+.img {
+  margin-left: 0px;
+}
 #app {
   height: 100vh;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+  position: relative;
 }
 #nav {
   width: 100%;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  padding-left: 20px;
   padding-bottom: 10px;
+  margin-top: 25px;
 }
 .navbar {
   display: inline;
   position: relative;
+  align-items: center;
+  padding-top: 35px;
 }
 .navbar-brand {
   display: inline;
-  max-width: 70px;
-  height: auto;
-  margin-left: 15px;
+  padding-bottom: 5px;
+  margin-top: 15px;
+  padding: 0.1em 0.1em;
+  white-space: nowrap;
+  height: 70px;
+  max-width: 200px;
+  margin-right: 20px;
 }
 .navbar-item {
   display: inline;
   max-width: 0em;
-  padding: 0.3em 0.5em;
+  padding: 1.2em 1.2em;
   border-radius: 9px;
   background-color: #00adee;
-  max-height: 3em;
-  margin: 0 .2em;
+  max-height: 4em;
+  margin: 0.4em 0.4em;
   color: white;
   text-transform: uppercase;
-  border: #8cc63f bold;
   white-space: nowrap;
+  margin-top: 15px;
+  height: 70px;
 }
 .navbar-item:hover {
   background-color: #8cc63f;
   color: white;
 }
-#app {
-  background-color: rgb(255, 255, 255);
-  position: relative;
-}
+
 .navbar-icon {
   display: inline;
   height: auto;
@@ -102,34 +99,154 @@
   margin-left: 20%;
   padding: auto;
 }
-.navbar-item-1 {
-  background-color: #00adee;
-}
-.navbar-item-2 {
-  background-color: #8cc63f ;
-}
-.navbar-item-3 {
-  background-color: #00adee;
-}
-@media screen and (max-width: 810px) {
+
+@media screen and (max-width: 960px) {
   #app {
     font-size: 10px;
   }
+  .navbar-item {
+    margin: 0px 20px;
+
+    padding-right: 30px;
+    padding-left: 30px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border-radius: 9px;
+    background-color: #00adee;
+    max-height: 3em;
+    margin: 0.3em 0.3em;
+    color: white;
+    text-transform: uppercase;
+    font-size: large;
+    white-space: nowrap;
+    margin-top: 15px;
+    height: 20px;
+  }
+
   .navbar-icon {
     max-width: 150px;
   }
   .navbar-brand {
     max-height: 40px;
   }
-  @media screen and (min-width: 650px) {
+}
+@media screen and(max-width: 970) {
+   .navbar-item {
+    margin: 0px 20px;
+
+    padding-right: 30px;
+    padding-left: 30px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-radius: 9px;
+    background-color: #00adee;
+    max-height: 3em;
+    margin: 0.3em 0.3em;
+    color: white;
+    text-transform: uppercase;
+    font-size: large;
+    white-space: nowrap;
+    margin-top: 15px;
+    height: 20px;
+  }
+
+  .navbar-icon {
+    max-width: 150px;
+  }
+  .navbar-brand {
+    max-height: 40px;
+  }
+}
+
+@media screen and (max-width: 895px) {
+  #app {
+    font-size: 10px;
+  }
+ 
+  .navbar-item {
+    padding-right: 30px;
+    padding-left: 30px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-radius: 9px;
+    background-color: #00adee;
+    max-height: 5em;
+    margin: 0.9em 0.9em;
+    color: white;
+    text-transform: uppercase;
+    font-size: medium;
+    white-space: nowrap;
+    margin-top: 25px;
+    height: 20px;
+
+  }
+
+  .navbar-icon {
+    max-width: 180px;
+  }
+  .navbar-brand {
+    max-height: 40px;
+  }
+
+  @media screen and (max-width: 650px) {
     #app {
       font-size: 13px;
     }
+    .navbar {
+        padding-top: 15px;
+
+    }
     .navbar-icon {
-      max-width: 200px;
+      max-width: 85px;
     }
     .navbar-brand {
-      max-height: 40px;
+      max-height: 50px;
+    }
+    .navbar-item {
+      padding: 0.1em 0.1em;
+      border-radius: 9px;
+      background-color: #00adee;
+      max-height: 2em;
+      margin: 0.4em 0.4em;
+      color: white;
+      text-transform: uppercase;
+      white-space: nowrap;
+      padding-top: 10px;
+      padding-bottom: 10px;
+
+      margin-top: 15px;
+      height: 20px;
+    }
+  }
+  @media screen and (min-width: 450px) {
+    #app {
+      font-size: 10px;
+    }
+    .navbar {
+        padding-top: 25px;
+
+    }
+
+    .navbar-icon {
+      max-width: 95px;
+    }
+    .navbar-brand {
+      max-width: 45px;
+      padding-bottom: 5px;
+    }
+    .navbar-item {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      border-radius: 9px;
+      background-color: #00adee;
+      max-height: 1em;
+      color: white;
+      text-transform: uppercase;
+      white-space: nowrap;
+      padding-right: 5px;
+      padding-left: 5px;
+      height: 10px;
+      font-size: small;
     }
   }
 }
